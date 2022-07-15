e10fb89f94405d5c17a705bbc0861f929796e626
Alondra87 <aa5887687@gmail.com>
2022-07-15 19:22:06 -0400

    Edited functionalities

--- module/checkBox.js
+++ module/checkBox.js
@@ -7,7 +7,7 @@ const CheckBoxesChecked = (event) => {
     event.target.nextElementSibling.classList.toggle('input-done');
     event.target.nextElementSibling.classList.toggle('done-task');
     event.target.parentNode.lastElementChild.classList.toggle('trash');
-    localStorage.setItem('mytodoList', JSON.stringify(list));
+    localStorage.setItem('mytodoList', JSON.stringify(listTasks));
   }
 };
 
